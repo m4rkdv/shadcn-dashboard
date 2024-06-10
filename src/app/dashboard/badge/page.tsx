843d4eee23912dc1cc6f83ecfb66f540ea01e6a6
Marcos CDV <yo.mark406@gmail.com>
2024-06-09 22:55:40 -0300

    badge component

---
+++ src/app/dashboard/badge/page.tsx
@@ -0,0 +1,14 @@
+import { Badge } from "@/components/ui/badge";
+
+export default function Page() {
+  return (
+    <div className="flex gap-4">
+      <Badge>default</Badge>
+      <Badge variant='secondary'>secondary</Badge>
+      <Badge variant='destructive'>destructive</Badge>
+      <Badge variant='outline'>outline</Badge>
+      <Badge variant='info'>info</Badge>
+      <Badge variant='success' capitalize>success</Badge>
+    </div>
+  );
+}
