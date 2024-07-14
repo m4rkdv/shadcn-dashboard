a824d81a0eed124fa42fbc899e0ddb1aa1ea8065
Marcos CDV <yo.mark406@gmail.com>
2024-07-14 20:15:43 -0300

    data table | row w/actions

--- src/app/dashboard/data-table/columns.tsx
+++ src/app/dashboard/data-table/columns.tsx
@@ -1,8 +1,18 @@
 "use client"
-
-import { Badge } from "@/components/ui/badge"
-import { Payment } from "@/data/payments.data"
-import { ColumnDef } from "@tanstack/react-table"
+import { Badge } from "@/components/ui/badge";
+import { Payment } from "@/data/payments.data";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { toast } from "sonner";
 
 export const columns: ColumnDef<Payment>[] = [
     {
@@ -15,10 +25,10 @@ export const columns: ColumnDef<Payment>[] = [
         cell: ({ row }) => {
             const status = row.getValue("status") as string;
             const variant = {
-                pending:"secondary",
-                processing:"info",
-                success:"success",
-                failed:"destructive",
+                pending: "secondary",
+                processing: "info",
+                success: "success",
+                failed: "destructive",
             }[status] ?? ("default") as any;
             return <Badge variant={variant}>{status}</Badge>
         }
@@ -39,5 +49,39 @@ export const columns: ColumnDef<Payment>[] = [
     {
         accessorKey: "email",
         header: "Email",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <DotsHorizontalIcon className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => {
+                                navigator.clipboard.writeText(payment.id);
+                                toast("Payment ID copied to clipboard",{
+                                    position:"top-right",
+                                });
+                            }
+                            }
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
     },
 ]
